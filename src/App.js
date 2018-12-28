f5da7b230c16eb8c89457af941fd84e80af2d7d3
haobin <bin.hao@roadefend.com>
2018-12-28 12:00:45 +0800

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          首页
+          我的首页
         </header>
       </div>
     );
