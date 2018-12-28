b407e2fe3884387309862938828f9df222d94d7c
haobin <bin.hao@roadefend.com>
2018-12-28 10:33:19 +0800

    创建docs目录

---
+++ docs/precache-manifest.8d19b81656652967928218b02197b24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6f372c2b2821554813f7",
+    "url": "./static/js/main.6f372c2b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "./static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "6f372c2b2821554813f7",
+    "url": "./static/css/main.6c365326.chunk.css"
+  },
+  {
+    "revision": "c6837bcae1827814f9a7f6a90d3c7ded",
+    "url": "./index.html"
+  }
+];
